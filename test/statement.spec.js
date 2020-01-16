a70ca2c8241c0c55ebf3651b419eece55a084e93
Kevin Kelly <kvinklly@gmail.com>
2020-01-16 16:25:43 -0600

    enhance testing experience - add tests for simpler inputs - add running tests in watch mode

--- test/statement.spec.js
+++ test/statement.spec.js
@@ -4,10 +4,12 @@ import invoices from "../data/invoices.json"
 import plays from "../data/plays.json"
 
 describe('#statement()', function() {
-  var invoice;
+  var invoice, comedyInvoice, tragedyInvoice
 
   beforeEach(function() {
-    invoice = invoices[0];
+    invoice = invoices[0],
+    comedyInvoice = invoices[1],
+    tragedyInvoice = invoices[2]
   });
 
   context('with valid invoice and play', function() {
@@ -19,8 +21,26 @@ describe('#statement()', function() {
 Amount owed is $1,640.00
 You earned 47 credits
 `
-      const myStatement = statement(invoice, plays);
+      const myStatement = statement(invoice, plays)
       expect(myStatement).to.equal(expectedResult)
+    })
+  })
+
+  context('with only a comedy', function() {
+    it ('should return the correct amounts', function() {
+      const myStatement = statement(comedyInvoice, plays)
+      expect(myStatement).to.include('57 seats')
+      expect(myStatement).to.include('Amount owed is $666.00')
+      expect(myStatement).to.include('You earned 38 credits')
+    })
+  })
+
+  context('with only a tragedy', function() {
+    it ('should return the correct amounts', function() {
+      const myStatement = statement(tragedyInvoice, plays)
+      expect(myStatement).to.include('13 seats')
+      expect(myStatement).to.include('Amount owed is $400.00')
+      expect(myStatement).to.include('You earned 0 credits')
     })
   })
 });
